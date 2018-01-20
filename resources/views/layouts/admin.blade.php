d0b8feb3bd65d80556fc689086265fc15a80098f
shairadelacruz <shairadcsofia@gmail.com>
2018-01-20 09:42:18 +0800

    Generic name

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -67,7 +67,7 @@
                 <a href="javascript:void(0);" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"></a>
                 <a href="javascript:void(0);" class="bars"></a>
                 <a class="navbar-brand" href="index.html">
-                AMY - Bookkeeping and Project Monitoring System
+                Bookkeeping and Project Monitoring System
                 </a>
             </div>
             <div class="collapse navbar-collapse" id="navbar-collapse">
